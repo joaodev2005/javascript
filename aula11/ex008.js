0584f443cdd9a7d52f61c20a1b7f06f42821e854
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-06-02 12:04:27 -0300

    ex JS

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 12
+console.log(`a velocidade e ${vel}km/h`)
+if (vel > 60) {
+    console.log('voce ultrapassou a velocidade permitida. MULTADO!')
+} else {
+    console.log('parabens continue assim subordinado')
+}
+console.log('dirija sempre usando cinto de segurança')
+
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var país = 'Servia'
+
+console.log(`vivendo no ${país}`)
+
+if (país != 'Brasil') {
+    console.log('voce e estrangeiro')
+}else {
+    console.log ('voce e brasileiro')
+}
