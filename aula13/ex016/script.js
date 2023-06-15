be37c779e235cc0e6cf21ca1f65ec7b2d8e7cd32
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-06-15 11:50:15 -0300

    exercicios

--- aula13/ex016/script.js
+++ aula13/ex016/script.js
@@ -4,16 +4,45 @@ function verificar() {
     var fano = document.getElementById('txtano')
     var res = document.getElementById('res')
     if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('[ERRO]verifique os dados')
-    }else {
-        var fsex = document.getElementById('radsex')
+        window.alert('[ERRO] verifique os dados')
+    } else {
+        var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Homem'
-        }else if (fsex[1].checked) {
+            if (idade >= 0 && idade < 10) {
+                //crianca
+                img.setAttribute('src', 'bebemasculino.jpg')
+            } else if (idade >= 10 && idade < 21) {
+                //jovem
+                img.setAttribute('src', 'jovenmas.jpg')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adultomas.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idosomas.webp')
+            }
+        } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //crianca
+                img.setAttribute('src', 'criancafem.jpg')
+            } else if (idade >= 10 && idade < 21) {
+                //jovem
+                img.setAttribute('src', 'jovenfem.webp')
+            } else if ( idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adultafem.webp')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idosafem.avif')
+            }
         }
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.innerHTML = `detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
