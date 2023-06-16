cf776d26365361359d61e5d8cc92717ac988343d
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-06-16 10:27:35 -0300

    repetição

---
+++ aula14/ambiente.js
@@ -0,0 +1,29 @@
+
+var c = 1
+
+do {
+    console.log(`passo ${c}`)
+    c++
+}while (c <= 6)
+
+
+
+
+/*
+var c = 1
+
+while(c <= 6) {
+    console.log(`passo ${c}`)
+    c = c + 1
+}
+*/
+
+/*
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+*/
+
