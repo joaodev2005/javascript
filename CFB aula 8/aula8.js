cbfddc868be98cf299770a9cf0261ca0d31a2dcc
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-07-03 11:10:24 -0300

    operadores bitwise

---
+++ CFB aula 8/aula8.js
@@ -0,0 +1,23 @@
+//OPERADORES BITWISE
+// operadores & | ^ << ou >>
+
+let n1 = 20
+res = n1 << 2
+
+console.log(res)
+
+
+
+
+
+
+
+
+
+// let n1, n2
+
+// n1 = 13
+// n2 = 14
+// res = n1 | n2
+
+// console.log(res)
