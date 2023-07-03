8fa6445e2fc1e876eee4825c007694df2d673e07
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-07-03 11:50:59 -0300

    pré e pós incremento

---
+++ CFB aula 9/aula9.js
@@ -0,0 +1,18 @@
+//PRE INCREMENTO e PÓS INCREMENTO:  -- decremento    ++ incremento    - negação      + concatenação  
+
+let n1 = 10
+let n2 = 20
+res = n1 + n2
+
+console.log('A soma de ' + n1 + ' e ' + n2 + ' e igual a: ' + res)
+
+
+
+
+
+// let n1 = 10
+
+
+// console.log(++n1)
+// console.log(n1--)
+// console.log(n1)
