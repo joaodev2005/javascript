f7dc9edf8d2a75733061ea744758d1644653abbc
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-07-03 12:41:21 -0300

    operador ternário

---
+++ CFB aula 10/aula10.js
@@ -0,0 +1,37 @@
+//OPERADOR TERNÁRIO  ?  =>    ( Teste lógico ? se verdadeiro : se falso )
+
+
+
+
+// let banco = 'I'
+
+// res = (banco == 'A' ? 'Ativo' : "Inativo")
+// console.log(res)
+ 
+
+// let num1 = 10
+// let num2 = 9
+
+// res = (num1 > num2 ? 'Verdadeiro' : "Falso")
+// console.log(res)
+
+
+//COM TERNARIO
+// let num = 3
+// res = (!(num%2) ? 'Par' : 'Impar')
+// console.log(res)
+
+//0 = False 
+//1 = True
+
+
+//NUMERO PAR ou IMPAR SEM TERNARIO
+// let num = 10
+
+// let res = num % 2
+
+// if(res == 0) {
+//     console.log('Par')
+// } else {
+//     console.log('Impar')
+// }
