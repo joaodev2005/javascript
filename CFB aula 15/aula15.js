dbe2d09b83e0196fe1be91bce6cd47f260c7bd9d
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-07-04 10:30:06 -0300

    loop for

---
+++ CFB aula 15/aula15.js
@@ -0,0 +1,15 @@
+//LOOP   FOR
+
+//CONTADOR NUMERO: IMPAR/PAR
+
+console.log('INICIO')
+
+for(let i = 0; i < 100; i++){
+    if(i % 2 == 0) {
+        console.log(`${i} - Par`)
+    }else {
+        console.log(`${i} - Impar`)
+    }
+}
+
+console.log('FIM')
