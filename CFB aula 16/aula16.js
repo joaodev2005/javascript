ba48adb198fd28f53d3130f1d8531bd690788d29
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-07-04 13:40:01 -0300

    for in e for of

---
+++ CFB aula 16/aula16.js
@@ -0,0 +1,21 @@
+//FOR IN / FOR OF
+
+const objs = document.getElementsByTagName('div')
+
+let num = [10, 20, 30, 40, 50]
+
+
+for(o of objs) {
+    console.log(o.innerHTML = 'curso')
+}
+
+
+for(o in objs){
+    console.log(objs[o].innerHTML)
+}
+
+
+
+// for (let i = 0;i < num.length; i++) {
+//     console.log(num[i])
+// }
