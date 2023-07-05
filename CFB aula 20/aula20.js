23a913d4219a225a0970193f73172d082dfc806b
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-07-05 12:57:15 -0300

    functions

---
+++ CFB aula 20/aula20.js
@@ -0,0 +1,16 @@
+//FUNCTIONS
+
+function mudarTexto() {
+    let d1 = document.getElementById('d1')
+    let d2 = document.getElementById('d2')
+    let d3 = document.getElementById('d3')
+
+    d1.innerHTML = 'programador'
+    d2.innerHTML = 'front'
+    d3.innerHTML = 'back'
+}
+
+
+for (var n = 0; n < 10; n++) {
+    soma()
+}
