098a846333e604e28b008b455167e28ad6b81180
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-07-05 15:20:18 -0300

    return

---
+++ CFB aula 21/aula21.js
@@ -0,0 +1,16 @@
+//FUNCTION COM RETORNO
+
+function canal() {
+    let n1 = 30
+    let n2 = 5
+    let res = n1 - n2
+    if ( res % 2== 0 ) {
+        return 'Par'
+    }else {
+        return 'Impar'
+    }
+}
+
+let res = canal()
+
+console.log(res)
