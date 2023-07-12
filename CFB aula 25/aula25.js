eb9e66083c9cdff75a5579a9edac9dbdb0908296
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-07-12 10:44:20 -0300

    Arrow Function

---
+++ CFB aula 25/aula25.js
@@ -0,0 +1,16 @@
+// ARROW FUNCTION
+
+const soma = (v1, v2) => {return v1 + v2}
+console.log(soma(30, 47))
+
+
+
+//sem chaves
+const add = t => t+50
+console.log(add(50))
+
+
+
+//um parâmetro
+const nome = n => {return n}
+console.log(nome('Java')) 
