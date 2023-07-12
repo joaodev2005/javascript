d99f0fa37e370a0220c13bfba64c0fb23b04100f
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-07-12 12:52:21 -0300

    Funções aninhadas

--- CFB aula 26/aula26.js
+++ CFB aula 26/aula26.js
@@ -1,2 +1,19 @@
 //  FUNÇÕES ANINHADAS
 
+const soma = (...valores) =>{
+    const somar = val => {
+        let res = 0
+        for(v of val )
+            res += v
+        return res  
+    }
+    return somar(valores)
+}
+
+console.log(soma(10, 5, 15, 3))
+
+
+
+
+valor = [10, 15, 40]
+console.log(soma(...valor))
