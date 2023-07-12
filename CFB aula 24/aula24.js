5beb00b876455aa5976d33d65c5d50311cd284ba
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-07-12 09:47:49 -0300

    Construtor

--- CFB aula 24/aula24.js
+++ CFB aula 24/aula24.js
@@ -1,6 +1,25 @@
 // FUNÇÕES ANÔNIMAS
 
-const num = function(v1, v2){
+
+//CONSTRUTOR
+
+const f = new Function('v1', 'v2', 'v3', 'return (v1+v2) / v3')
+
+console.log(f(20, 10, 12))
+
+
+
+
+
+
+
+const num = function(...valores){
     let res = 0
+    for (v of valores){
+        res += v
+    }
+    return res
 }
+
+console.log(num(10, 5))
 
