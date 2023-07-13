b0a1084ca759ca506d90eab73aa7de2e68032dca
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-07-13 12:28:07 -0300

    Map

---
+++ FCB aula 28/aula28.js
@@ -0,0 +1,35 @@
+// MAP
+
+
+const converterInt = (e) => parseInt(e)
+const dobrar = (e) => e*2
+let num = ['1', '2', '3', '4', '5'].map(dobrar)
+console.log(num)
+
+
+
+// const ele = document.getElementsByTagName('div')
+// const val = Array.prototype.map.call(ele, ({innerHTML}) => innerHTML)
+// console.log(val)
+
+
+// let el = document.getElementsByTagName('div')
+// el = [...el]
+// console.log(el)
+// el.map((e,i)=> {
+//     e.innerHTML = 'curso bom'
+// }) 
+
+
+// const cursos = ['HTML', 'Java', 'CSS', 'REACT', 'MySQL']
+// const c = cursos.map((elemento,indice) => {
+//     console.log(`Curso: ${elemento} - Posição: ${indice}`)
+// })
+
+
+
+
+
+
+
+
