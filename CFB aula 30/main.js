e94e2268c3ba572bb051cfe4c3fddcf5da534a78
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-07-17 09:43:59 -0300

    DOM - GetElementById

--- CFB aula 30/main.js
+++ CFB aula 30/main.js
@@ -1,3 +1,21 @@
+// DOM = GetElementById
 const dc1 = document.getElementById('c1')
+const dc2 = document.getElementById('c2')
+const dc3 = document.getElementById('c3')
+const dc4 = document.getElementById('c4')
+const dc5 = document.getElementById('c5')
+const dc6 = document.getElementById('c6')
 
-console.log(dc1)
+
+const arrayElementos = [dc1, dc2, dc3, dc4, dc5, dc6]
+
+for(d of arrayElementos) {
+    d.innerHTML = 'curso bom'
+}
+
+arrayElementos.map((e) => {
+    e.innerHTML = 'Dev mobile'
+    console.log(e)
+})
+
+console.log(arrayElementos)
