7243b01dffe8fc29f9bbca40c166363e5a6c9c7b
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-07-24 11:09:39 -0300

    createElement

--- CFB aula 40/aula40.js
+++ CFB aula 40/aula40.js
@@ -1,3 +1,22 @@
+// createElement
+
 const caixa1 = document.querySelector('#caixa1')
 const btn_c = [...document.querySelectorAll('.curso')]
 const c1_2 = document.querySelector('#c1_2')
+const cursos = ['HTTML', 'CSS', 'JavaScript', 'PHP', 'React', 'MySQL', 'MongoDB']
+
+cursos.map((el, indice) => {
+    const newElement = document.createElement('div')
+    newElement.setAttribute('id', `c${indice}`)
+    newElement.setAttribute('class', 'curso c1')
+    newElement.innerHTML = el
+    caixa1.appendChild(newElement)
+})
+
+// const newElement = document.createElement('div')
+// newElement.setAttribute('id', 'c7')
+// newElement.setAttribute('class', 'curso c1')
+// newElement.innerHTML = 'MongoDB'
+
+// caixa1.appendChild(newElement)
+
