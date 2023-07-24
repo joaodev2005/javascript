c08435e62824acc386e575298cf4a7546063f0d3
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-07-24 10:20:30 -0300

    Manipulando o DOM

--- CFB aula 39/aula39.js
+++ CFB aula 39/aula39.js
@@ -1,3 +1,22 @@
+//Relações do DOM
+
 const caixa1 = document.querySelector('#caixa1')
 const btn_c = [...document.querySelectorAll('.curso')]
+const c1_2 = document.querySelector('#c1_2')
 
+console.log(c1_2.parentNode.parentNode.children[4])
+
+console.log(caixa1.hasChildNodes())
+console.log(btn_c[0].hasChildNodes())
+console.log(btn_c[0].childNodes)
+
+
+console.log(btn_c[0].children.length > 0 ? 'Possui Filhos' : 'Não Possui Filhos')
+
+caixa1.children[0].innerHTML = 'teste'
+
+// if(caixa1.children.length > 0) {
+//     console.log('Possui Filhos')
+// }else{
+//     console.log('Não Possui filhos')
+// }
---
+++ CFB aula 40/aula40.js
@@ -0,0 +1,3 @@
+const caixa1 = document.querySelector('#caixa1')
+const btn_c = [...document.querySelectorAll('.curso')]
+const c1_2 = document.querySelector('#c1_2')
