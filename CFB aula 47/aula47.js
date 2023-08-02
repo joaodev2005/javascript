cd4bfe345aba38a90c0b59948e6f04d84e3d4459
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-08-02 09:36:23 -0300

    JS

--- CFB aula 47/aula47.js
+++ CFB aula 47/aula47.js
@@ -1,3 +1,5 @@
+//find
+
 const p_array = document.querySelector('#array')
 const txt_pesquisar = document.querySelector('#txt_pesquisar')
 const btnPesquisar = document.querySelector('#btnpesquisar')
---
+++ CFB aula 48/aula48.js
@@ -0,0 +1,21 @@
+//every
+
+const p_array = document.querySelector('#array')
+const btnVerificar = document.querySelector('#btnverificar')
+const resultado = document.querySelector('#resultado')
+
+const elementos_array = [9, 84, 1233, 94, 58, 333, 23, 133, 1633]
+p_array.innerHTML = elementos_array
+
+btnVerificar.addEventListener('click', (evt) => {
+    const ret = elementos_array.every((el, i) => {
+        if(el < 18){
+            resultado.innerHTML = `Array não conforme na Posição: ${i}`
+        }
+        return el >= 18
+    })
+    if(ret){
+        resultado.innerHTML = 'OK'
+    }
+    // console.log(ret)
+})
