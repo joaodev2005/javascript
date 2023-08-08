ac62694facc083f749a217f0eb5ecaf9be1c10e1
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-08-08 11:03:00 -0300

    mais sobre array

---
+++ CFB aula 52/aula52.js
@@ -0,0 +1,21 @@
+// array
+
+const caixa = document.getElementById('caixa')
+
+let cores = ['azul', 'rosa', 'vermelho', 'dourado', 'roxo',['claro','forte','medio']]
+let games = ['Undertale', 'Earthbound', 'The last Of Us', cores]
+
+// games.push('GTA V')
+// games.push('The Witcher 3')
+// games.push('Elden ring')
+
+// games.unshift('Fear and Hunger')
+// games.shift()
+
+console.log(games[3][5][2])
+
+games.map((el) => {
+    let p = document.createElement('p')
+    p.innerHTML = el
+    caixa.appendChild(p)
+})
